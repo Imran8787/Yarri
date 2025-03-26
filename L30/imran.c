22ea04164b67925e0c06358648cc05502dc14de3
imran8787 <imranbadesab87@gmail.com>
2025-03-26 13:18:14 +0530

    update

--- L30/imran.c
+++ L30/imran.c
@@ -36,4 +36,21 @@ int main() {
     printf("Result: %.2lf\n", result);
     return 0;
 }
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
 
